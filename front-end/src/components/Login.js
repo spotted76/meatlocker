fd1826c250fbade6779b61c494cba691e8f43a6a
Peter Fischer <spotted@gmail.com>
2020-01-14 16:06:12 -0500

    styling added, need to fix

--- front-end/src/components/Login.js
+++ front-end/src/components/Login.js
@@ -6,6 +6,8 @@ import Cookies from 'js-cookie';
 import LoginService from '../services/loginService';
 import Config from '../utils/config';
 
+import  './dialogs.css';
+
 function Login() {
 
   const [userToken, setUserToken] = useState(null);
@@ -69,21 +71,23 @@ function Login() {
           <h2>Login</h2>
         </div>
         <form onSubmit={onSubmit}>
-          <div className="auth_input">
-            <label htmlFor="username">username: </label>
-            <input name="username" {...username} required />
-          </div>
-          <div className="auth_input">
-            <label htmlFor="password">password: </label>
-            <input name="password" {...password} required />
-          </div>
-          <div className="auth_input">
-            <button>Submit</button>
-          </div>
-          <div className="authFooter">
-            <p>create an account</p>
+          <div className="inputs">
+            <div className="auth_input">
+              <label htmlFor="username">username: </label>
+              <input name="username" {...username} required />
+            </div>
+            <div className="auth_input">
+              <label htmlFor="password">password: </label>
+              <input name="password" {...password} required />
+            </div>
+            <div className="auth_input">
+              <button>Submit</button>
+            </div>
           </div>
         </form>
+        <div className="authFooter">
+          <p>create an account</p>
+        </div>
       </div>
     </div>
   );
